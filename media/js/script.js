c2ffafe92f40a019a5192d125e65bd1f5d6b420a
Vladisalv Mac <mvvminsk@gmail.com>
2020-11-29 18:53:32 +0300

    circle tool creation

--- media/js/script.js
+++ media/js/script.js
@@ -1,19 +1,51 @@
 var 
-  ctx         = canv.getContext("2d"), 
-  canv        = document.querySelector('#canvas');
+canv        = document.querySelector('#canvas'),
+ctx         = canv.getContext("2d");
 
 var
   pen         = document.querySelector('#pen'),
   eraser      = document.querySelector('#eraser'),
+  structure   = document.querySelector('#structure'),
+  circle      = document.querySelector('#circle'),
   saveDraw    = document.querySelector('#save'),
   replayDraw  = document.querySelector('#replay');
 
 var
-  colorBg     = document.querySelector('#colorBg'),
+  colorBg     = document.querySelector('#bgColor'),
   sizePen     = document.querySelector('#sizePen'),
-  colorPen    = document.querySelector('#colorPen');
+  colorPen    = document.querySelector('#penColor');
 
-
-canv.width = window.innerWidth;  
+  canv.width = window.innerWidth;  
 canv.height = window.innerHeight - 90; 
 
+
+
+
+// structure.addEventListener('mousedown', function structure(){
+//   canv.addEventListener('mousedown', function(e){
+//     ctx.fillStyle = colorPen.value; // Цвет квадрата по стандарту черн 
+//     ctx.arc(e.clientX, e.clientY, 30, 0, Math.PI * 2);
+//     ctx.fill();
+//   });
+// });
+
+// mouse
+
+circle.addEventListener('mousedown', function circleMouse(){
+  canv.addEventListener('mousedown', function(e){
+    ctx.fillStyle = colorPen.value; // Цвет квадрата по стандарту черн 
+    ctx.beginPath();
+    ctx.arc(e.clientX, e.clientY, 30, 0, Math.PI * 2);
+    ctx.fill();
+  });
+});
+
+// touch
+circle.addEventListener('touchstart', function circleTouch(){
+  canv.addEventListener('touchstart', function(e){
+    ctx.fillStyle = colorPen.value; // Цвет квадрата по стандарту черн 
+    ctx.beginPath();
+    ctx.arc(e.clientX, e.clientY, 30, 0, Math.PI * 2);
+    ctx.fill();
+  });
+});
